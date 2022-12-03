2079fb4e261ebd0e288809c1b58a21ba8059209d
Sea Lion <aungchitmin1112000@gmail.com>
2022-12-03 11:24:57 +0630

    changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,11 @@ const port = process.env.PORT || 5000;
 const pinRoute = require("./routes/pins");
 const userRoute = require("./routes/users");
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({path: __dirname+'/.env'});
+}
+
+
 const app = express();
 
 mongoose
@@ -22,5 +27,12 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/pins", pinRoute);
 app.use("/api/users", userRoute);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+  })
+}
 
 app.listen(port, () => console.log(`server started on port ${port}`));
